5c9348b231aa0f684eede12c7df36b1e4a22c8c7
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-01-12 11:00:38 -0300

    Subindo projeto no git

---
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
@@ -0,0 +1,121 @@
+﻿
+$(document).ready(function () {
+    if (obj) {
+        $('#formCadastro #Nome').val(obj.Nome);
+        $('#formCadastro #CEP').val(obj.CEP);
+        $('#formCadastro #Email').val(obj.Email);
+        $('#formCadastro #Sobrenome').val(obj.Sobrenome);
+        $('#formCadastro #Nacionalidade').val(obj.Nacionalidade);
+        $('#formCadastro #Estado').val(obj.Estado);
+        $('#formCadastro #Cidade').val(obj.Cidade);
+        $('#formCadastro #Logradouro').val(obj.Logradouro);
+        $('#formCadastro #Telefone').val(obj.Telefone);
+        $('#formCadastro #CPF').val(obj.CPF);
+
+    }
+
+    $('#formCadastro').submit(function (e) {
+        e.preventDefault();
+
+        if (!TestaCPF(document.querySelector('#CPF').value)) {
+            ModalDialog("Ocorreu um erro", "O número do CPF é invalido.");
+            return false;
+        }
+        
+        $.ajax({
+            url: urlPost,
+            method: "POST",
+            data: {
+                "NOME": $(this).find("#Nome").val(),
+                "CEP": $(this).find("#CEP").val(),
+                "Email": $(this).find("#Email").val(),
+                "Sobrenome": $(this).find("#Sobrenome").val(),
+                "Nacionalidade": $(this).find("#Nacionalidade").val(),
+                "Estado": $(this).find("#Estado").val(),
+                "Cidade": $(this).find("#Cidade").val(),
+                "Logradouro": $(this).find("#Logradouro").val(),
+                "Telefone": $(this).find("#Telefone").val(),
+                "CPF": $(this).find("#CPF").val(),
+            },
+            error:
+            function (r) {
+                if (r.status == 400)
+                    ModalDialog("Ocorreu um erro", r.responseJSON);
+                else if (r.status == 500)
+                    ModalDialog("Ocorreu um erro", "Ocorreu um erro interno no servidor.");
+            },
+            success:
+            function (r) {
+                ModalDialog("Sucesso!", r)
+                $("#formCadastro")[0].reset();                                
+                window.location.href = urlRetorno;
+            }
+        });
+    })
+    
+})
+
+function ModalDialog(titulo, texto) {
+    var random = Math.random().toString().replace('.', '');
+    var texto = '<div id="' + random + '" class="modal fade">                                                               ' +
+        '        <div class="modal-dialog">                                                                                 ' +
+        '            <div class="modal-content">                                                                            ' +
+        '                <div class="modal-header">                                                                         ' +
+        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
+        '                    <h4 class="modal-title">' + titulo + '</h4>                                                    ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-body">                                                                           ' +
+        '                    <p>' + texto + '</p>                                                                           ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-footer">                                                                         ' +
+        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
+        '                                                                                                                   ' +
+        '                </div>                                                                                             ' +
+        '            </div><!-- /.modal-content -->                                                                         ' +
+        '  </div><!-- /.modal-dialog -->                                                                                    ' +
+        '</div> <!-- /.modal -->                                                                                        ';
+
+    $('body').append(texto);
+    $('#' + random).modal('show');
+}
+
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+
+    strCPF = strCPF.replace('.', '').replace('.', '').replace('-', '');
+
+    if (strCPF == "00000000000") return false;
+
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+}
+
+function fMasc(objeto, mascara) {
+    obj = objeto
+    masc = mascara
+    setTimeout("fMascEx()", 1)
+}
+function fMascEx() {
+    obj.value = masc(obj.value)
+}
+
+function mCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    return cpf
+}
